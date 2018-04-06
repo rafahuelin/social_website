c7b572347f710d1dbea0a37fdd55578ee8f80a61
Rafa <rafahuelin@gmail.com>
2018-04-06 13:35:51 +0200

    setup password change with customized templates

--- account/urls.py
+++ account/urls.py
@@ -1,5 +1,5 @@
 from django.contrib.auth import views as auth_views
-from django.urls import path
+from django.urls import path, reverse_lazy
 
 from . import views
 
@@ -12,4 +12,11 @@ urlpatterns = [
     # login / logout urls
     path('login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('logout-then-login/', auth_views.logout_then_login, name='logout_then_login'),
+
+    # change password urls
+    path('password-change/',
+         auth_views.PasswordChangeView.as_view(success_url=reverse_lazy('account:password_change_done')),
+         name='password_change'),
+    path('password_change/done/', auth_views.PasswordChangeDoneView.as_view(), name='password_change_done'),
 ]
